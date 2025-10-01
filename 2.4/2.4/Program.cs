de14fa178356078e1d45e541ece024c31b130cf9
andrey121-212 <andrey.glent@mail.ru>
2025-10-01 09:26:12 +0300

    Add files via upload

---
+++ 2.4/2.4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+interface IDrawable
+{
+    void Draw();
+}
+
+class Circle : IDrawable
+{
+    public void Draw()
+    {
+        Console.WriteLine("Рисую треугольник");
+    }
+}
+
+class Rectangle : IDrawable
+{
+    public void Draw()
+    {
+        Console.WriteLine("Рисую круг");
+    }
+}
+
+class Triangle : IDrawable
+{
+    public void Draw()
+    {
+        Console.WriteLine("Рисую квадрат");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        IDrawable[] shapes = new IDrawable[]
+        {
+            new Circle(),
+            new Rectangle(),
+            new Triangle()
+        };
+
+        foreach (IDrawable shape in shapes)
+        {
+            shape.Draw();
+        }
+    }
+}
