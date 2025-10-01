c216aef1f7ce004cb76051c18b0683c82732c5e7
andrey121-212 <andrey.glent@mail.ru>
2025-10-01 09:19:16 +0300

    Add files via upload

---
+++ 2.1/2.1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+class Person
+{
+    private string name;
+    private int age;
+    private string address;
+
+    // Методы для установки значений
+    public void SetName(string name)
+    {
+        this.name = name;
+    }
+    public void SetAge(int age)
+    {
+        this.age = age;
+    }
+    public void SetAddress(string address)
+    {
+        this.address = address;
+    }
+
+    // Методы для получения значений
+    public string GetName()
+    {
+        return name;
+    }
+    public int GetAge()
+    {
+        return age;
+    }
+
+    public string GetAddress()
+    {
+        return address;
+    }
+
+    // Метод для вывода информации
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Имя: {name}, Возраст: {age}, Адрес: {address}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Person person1 = new Person();
+        person1.SetName("Стен");
+        person1.SetAge(52);
+        person1.SetAddress("Нальчик");
+
+        person1.DisplayInfo();
+    }
+}
